37b7c0696b9bce4bab0114d812a7b601658b47f8
Esteban Ordenavia <esteban.ordenavia@flexia.com.ar>
2022-08-29 20:05:59 -0300

    detalles

--- ceb/views.py
+++ ceb/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-<<<<<<< Updated upstream
-=======
+
 from .forms import CustomUserCreationForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login 
@@ -13,18 +12,16 @@ def registro(request):
 	data = {
 	'form': CustomUserCreationForm()
 	}
->>>>>>> Stashed changes
+
 
 def Home(request):
     return render(request, 'home.html')
 
-<<<<<<< Updated upstream
+
 def Registro(request):
-    return render(request, 'registro.html')
-=======
 	return render(request, 'usuarios/registro.html', data)
 
 
 def Historia(request):
     return render(request, 'historia.html')
->>>>>>> Stashed changes
+
